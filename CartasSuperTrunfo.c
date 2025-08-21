e36e7321a016c6fec6636e5fd241dbb9f2a3ff11
programacaomateusc <202508583755@alunos.estacio.br>
2025-08-21 00:55:06 +0000

    novo commit

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -9,7 +9,10 @@ int main() {
     float area1;
     float pib1;
     int pontosTuristicos1;
+    float densidadepopulacional1;
+    float pibpercapto1;
 
+    
     char estado2;
     char codigo2[4];
     char nomeCidade2[50];
@@ -17,6 +20,10 @@ int main() {
     float area2;
     float pib2;
     int pontosTuristicos2;
+    float densidadepopulacional2;
+    float pibpercapto2;
+
+    
 
     printf("Digite os dados da carta1:\n");
 
@@ -64,6 +71,14 @@ int main() {
     printf("Pontos turísticos: ");
     scanf("%d", &pontosTuristicos2);
 
+pibpercapto1 = pib1 / populacao1;
+densidadepopulacional1 = populacao1 / area1;
+
+
+pibpercapto2 = pib2 / populacao2;
+densidadepopulacional2 = populacao2 / area2;
+
+
     printf("\nCarta 1:\n");
 printf("Estado: %c\n", estado1);
 printf("Código: %s\n", codigo1);
@@ -72,6 +87,8 @@ printf("População: %d\n", populacao1);
 printf("Área: %.2f km²\n", area1);
 printf("PIB: %.2f bilhões de reais\n", pib1);
 printf("Número de Pontos Turísticos: %d\n", pontosTuristicos1);
+printf("A densidade poluacional e: %.2f\n", densidadepopulacional1);
+printf("o pib percapto e: %.2f\n", pibpercapto1);
 
 printf("\nCarta 2:\n");
 printf("Estado: %c\n", estado2);
@@ -81,6 +98,10 @@ printf("População: %d\n", populacao2);
 printf("Área: %.2f km²\n", area2);
 printf("PIB: %.2f bilhões de reais\n", pib2);
 printf("Número de Pontos Turísticos: %d\n", pontosTuristicos2);
+printf("A densidade poluacional e: %.2f\n", densidadepopulacional2);
+printf("o pib percapto e: %.2f\n", pibpercapto2);
+
+
 
     return 0;
 }
